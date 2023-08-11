65255da24345c90cb2bc0821c7a7a604686146be
YasevichAlexandr <114285174+YasevichAlexandr@users.noreply.github.com>
2023-08-11 13:35:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fig1;
+using Fig2;
+using Fig3;
+using Fig4;
+using Fig5;
+using Fig6;
+using Fig7;
+using Fig8;
+using Fig9;
+using Fig10;
+using Fig11;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+       
+        Triangle[] triangles = new Triangle[]
+        {
+            new RightTriangle(new Point(3, 0), new Point(0, 0), new Point(0, 4), LineType.Dashed, FillColor.Green),
+            new EquilateralTriangle(new Point(0, 0), new Point(1, 0), new Point(0.5, Math.Sqrt(3) / 2), LineType.Dotted, FillColor.Yellow),
+            new IsoscelesTriangle(new Point(0, 0), new Point(4, 0), new Point(2, 3), LineType.Dotted, FillColor.Yellow),
+            new ArbitraryTriangle(new Point(0, 0), new Point(3, 0), new Point(1, 4), LineType.Solid, FillColor.Blue)
+        };
+
+        double totalArea = CalculateTotalTrianglesArea(triangles);
+
+        Rectangle rectangle = new Rectangle()
+        {
+            LineType = LineType.Solid,
+            FillColor = FillColor.Yellow,
+            TopLeft = new Point(1, 1),
+            Width = 5,
+            Height = 3
+        };
+
+        Circle circle = new Circle()
+        {
+            LineType = LineType.Dashed,
+            FillColor = FillColor.Red,
+            Center = new Point(3, 3),
+            Radius = 2
+        };
+
+        double totalTrianglesArea = CalculateTotalTrianglesArea(triangles);
+        double rectangleArea = rectangle.CalculateArea();
+        double circleArea = circle.CalculateArea();
+
+        double TotalArea = totalTrianglesArea + rectangleArea + circleArea;
+
+        Console.WriteLine("Total Area: " + TotalArea);
+
+        Console.ReadLine();
+    }
+    static double CalculateTotalTrianglesArea(Triangle[] triangles)
+    {
+        double totalTrianglesArea = 0;
+
+        foreach (Triangle triangle in triangles)
+        {
+            totalTrianglesArea += triangle.CalculateArea();
+        }
+
+        return totalTrianglesArea;
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fig1;
+using Fig2;
+using Fig3;
+using Fig4;
+
+namespace Fig5
+{
+    public class Triangle : IShape
+    {
+        public LineType LineType { get; set; }
+        public FillColor FillColor { get; set; }
+
+        protected Point PointA
+        {
+            get;
+            set;
+        }
+        protected Point PointB
+        {
+            get;
+            set;
+        }
+        protected Point PointC
+        {
+            get;
+            set;
+        }
+        public bool IsTriangle()
+        {
+            double sideAB = CalculateDistance(PointA, PointB);
+            double sideBC = CalculateDistance(PointB, PointC);
+            double sideCA = CalculateDistance(PointC, PointA);
+
+            return sideAB + sideBC > sideCA && sideBC + sideCA > sideAB && sideCA + sideAB > sideBC;
+        }
+
+        private bool IsPythagorean(double side1, double side2, double hypotenuse)
+        {
+            return Math.Abs(Math.Pow(side1, 2) + Math.Pow(side2, 2) - Math.Pow(hypotenuse, 2)) < double.Epsilon;
+        }
+        public bool IsRightTriangle()
+        {
+            if (!IsTriangle())
+            {
+                return false;
+            }
+            double sideAB = CalculateDistance(PointA, PointB);
+            double sideBC = CalculateDistance(PointB, PointC);
+            double sideCA = CalculateDistance(PointC, PointA);
+
+            return IsPythagorean(sideAB, sideBC, sideCA) || IsPythagorean(sideBC, sideCA, sideAB) || IsPythagorean(sideCA, sideAB, sideBC);
+        }
+
+        public bool IsEquilateralTriangle()
+        {
+            if (!IsTriangle())
+            {
+                return false;
+            }
+
+            double sideAB = CalculateDistance(PointA, PointB);
+            double sideBC = CalculateDistance(PointB, PointC);
+            double sideCA = CalculateDistance(PointC, PointA);
+
+
+            //??? double.Epsilon ???
+            //return (Math.Abs(sideAB - sideBC)) < double.Epsilon && (Math.Abs(sideBC - sideCA)) < double.Epsilon;
+            return Math.Abs(sideAB - sideBC) < 0.0001 && Math.Abs(sideBC - sideCA) < 0.0001;
+        }
+
+        public bool IsIsoscelesTriangle()
+        {
+            if (!IsTriangle())
+            {
+                return false;
+            }
+            double sideAB = CalculateDistance(PointA, PointB);
+            double sideBC = CalculateDistance(PointB, PointC);
+            double sideCA = CalculateDistance(PointC, PointA);
+
+            return (sideAB - sideBC) < double.Epsilon || (sideBC - sideCA) < double.Epsilon || (sideCA - sideAB) < double.Epsilon;
+        }
+        public virtual double CalculateArea()
+        {
+            return 0;
+        }
+        public double CalculateDistance(Point point1, Point point2)
+        {
+            return Math.Sqrt(Math.Pow(point2.X - point1.X, 2) + Math.Pow(point2.Y - point1.Y, 2));
+        }
+    }
+
+}
